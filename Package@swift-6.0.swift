3dbcd5d0470df59046acfb268c7a242eadc2f20c
maximkrouk <id.maximkrouk@gmail.com>
2026-03-10 07:20:41 +0100

    feat: Update KeyPathsExtensions

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -57,5 +57,6 @@ let package = Package(
 				.target(name: "_ResettableDebugging"),
 			]
 		),
-	]
+	],
+	swiftLanguageModes: [.v6]
 )
